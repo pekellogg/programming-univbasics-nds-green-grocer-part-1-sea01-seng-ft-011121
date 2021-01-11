46432ab4363f840e19f96e9af3acf4467e7de413
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-11 02:22:00 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -27,20 +27,19 @@ require 'pry'
 #   receipt.uniq
 # end
 
-
 def consolidate_cart(cart)
-	new_cart = []
-	cart.map do |og_e|
-	if !find_item_by_name_in_collection(og_e[:item], new_cart)
-		new_cart.push(og_e)
-		og_e[:count] = 1
-	else
-		new_cart.map do |e|
-		if og_e[:item] == e[:item]
-		e[:count] += 1
-		end
-	end
-end
+ new_cart = []
+ cart.map do |og_e|
+   if !find_item_by_name_in_collection(og_e[:item], new_cart)
+     new_cart.push(og_e)
+     og_e[:count] = 1
+   else
+     new_cart.map do |e|
+     if og_e[:item] == e[:item]
+     e[:count] += 1
+     end
+   end
+ end
 end
 print new_cart
 end
